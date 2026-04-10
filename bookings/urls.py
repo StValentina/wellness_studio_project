08340b3127059aaf478f1595621c5c1c559a376f
StValentina <valentina.st999@gmail.com>
2026-04-10 09:59:50 +0300

    finished with my booking functionality

--- bookings/urls.py
+++ bookings/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from bookings.views import BookClassView, CancelBookingView
+from bookings.views import BookClassView, CancelBookingView, MyBookingsView
 
 urlpatterns = [
+    path('my-bookings/', MyBookingsView.as_view(), name='my-bookings'),
     path('book/<int:pk>/', BookClassView.as_view(), name='book-class'),
     path('cancel/<int:pk>/', CancelBookingView.as_view(), name='cancel-booking'),
 ]
--- bookings/views.py
+++ bookings/views.py
@@ -1,6 +1,8 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render, get_object_or_404, redirect
+from django.utils import timezone
 from django.views import View
+from django.views.generic import TemplateView
 
 from bookings.models import Booking
 from studio_classes.models import StudioClass
@@ -29,3 +31,45 @@ class CancelBookingView(LoginRequiredMixin, View):
         booking.delete()
 
         return redirect('my_profile', pk=pk)
+
+class MyBookingsView(LoginRequiredMixin, TemplateView):
+    template_name = 'bookings/my-bookings.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        today = timezone.localdate()
+        now_time = timezone.localtime().time()
+
+        user_bookings = Booking.objects.filter(
+            user=self.request.user
+        ).select_related('booked_class')
+
+        upcoming_bookings = []
+        past_bookings = []
+
+        for booking in user_bookings:
+            booked_class = booking.booked_class
+
+            if booked_class.class_date > today:
+                upcoming_bookings.append(booking)
+            elif booked_class.class_date < today:
+                past_bookings.append(booking)
+            else:
+                if booked_class.start_time >= now_time:
+                    upcoming_bookings.append(booking)
+                else:
+                    past_bookings.append(booking)
+
+        upcoming_bookings.sort(
+            key=lambda b: (b.booked_class.class_date, b.booked_class.start_time)
+        )
+        past_bookings.sort(
+            key=lambda b: (b.booked_class.class_date, b.booked_class.start_time),
+            reverse=True
+        )
+
+        context['upcoming_bookings'] = upcoming_bookings
+        context['past_bookings'] = past_bookings
+
+        return context
