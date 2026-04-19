67140cb1e0aa7464d667bd70b6494af11973a535
StValentina <valentina.st999@gmail.com>
2026-04-19 19:36:57 +0300

    edit signals for booking

--- bookings/signals.py
+++ bookings/signals.py
@@ -1,5 +1,6 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from django.utils.autoreload import logger
 
 from .models import Booking
 from .tasks import send_booking_confirmation_email
@@ -8,7 +9,10 @@ from .tasks import send_booking_confirmation_email
 @receiver(post_save, sender=Booking)
 def booking_created_send_email(sender, instance, created, **kwargs):
     if created:
-        send_booking_confirmation_email.delay(
-            instance.user.email,
-            instance.booked_class.class_title,
-        )
+        try:
+            send_booking_confirmation_email.delay(
+                instance.user.email,
+                instance.booked_class.class_title,
+            )
+        except Exception as e:
+            logger.error(f"Failed to queue booking email: {e}")
